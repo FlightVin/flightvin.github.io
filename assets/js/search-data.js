f843e580093305d772ec9ede4498122457772c13
FlightVin <35373409+FlightVin@users.noreply.github.com>
2024-12-30 17:22:27 +0000

    Deploying to gh-pages from @ FlightVin/flightvin.github.io@c06ba9cf54ffadc0b1e9acd87be586ee51b2ece5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,26 +30,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "A resume with salient points on my experience and a few projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "dropdown-publications",
               title: "publications",
